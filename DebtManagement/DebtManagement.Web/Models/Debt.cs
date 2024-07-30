1de04d1873a1a48b8959123438f9265b32593f90
bkaand <kaandurm@gmail.com>
2024-07-30 17:06:23 +0300

    nothing

--- DebtManagement/DebtManagement.Web/Models/Debt.cs
+++ DebtManagement/DebtManagement.Web/Models/Debt.cs
@@ -1,24 +1,4 @@
-/*using System;
 
-namespace DebtManagement.Web.Models
-{
-    public class Debt
-    {
-        public int DebtId { get; set; }
-        public string UserId { get; set; }
-        public string DebtType { get; set; }
-        public decimal DebtAmount { get; set; }
-        public int Installments { get; set; }
-        public decimal RemainingAmount { get; set; }
-        public decimal EarlyClosingAmount { get; set; }
-        public decimal InterestRateMonthly { get; set; }
-        public decimal InterestRateYearly { get; set; }
-        public decimal InsuranceAmount { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public User User { get; set; }
-    }
-}
-*/
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
