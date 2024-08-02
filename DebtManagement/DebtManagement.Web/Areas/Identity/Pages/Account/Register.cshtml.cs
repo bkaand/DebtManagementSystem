23e292431fbad78e4d825d7789ea0c7876ef902b
bkaand <kaandurm@gmail.com>
2024-08-02 14:08:27 +0300

    register fixed

---
+++ DebtManagement/DebtManagement.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,60 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using DebtManagement.Web.Models;
+using DebtManagement.Web.DTOs;
+using DebtManagement.Web.Entities;
+
+namespace DebtManagement.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+
+        public RegisterModel(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<RegisterModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public RegisterDto Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = Input.Email, Email = Input.Email, ClientName = Input.ClientName };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ DebtManagement/DebtManagement.Web/DTOs/RegisterDTO.cs
@@ -0,0 +1,10 @@
+namespace DebtManagement.Web.DTOs
+{
+    public class RegisterDto
+    {
+        public string ClientName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
