9b87f5b5bb614817ec1608899cb3d09a47a13a2a
bkaand <kaandurm@gmail.com>
2024-08-06 09:26:39 +0300

    removed comments

--- DebtManagement/DebtManagement.Web/Mappings/MappingProfile.cs
+++ DebtManagement/DebtManagement.Web/Mappings/MappingProfile.cs
@@ -1,26 +1,3 @@
-/*using AutoMapper;
-using DebtManagement.Web.DTOs;
-using DebtManagement.Web.Entities;
-
-namespace DebtManagement.Web.Mappings
-{
-    public class MappingProfile : Profile
-    {
-        public MappingProfile()
-        {
-            //mappings go here
-            CreateMap<Client, ClientDTO>().ReverseMap();
-            CreateMap<Debt, DebtDTO>().ReverseMap();
-           // CreateMap<Income, IncomeDTO>().ReverseMap();
-            CreateMap<Payment, PaymentDTO>().ReverseMap(); 
-            CreateMap<Income, IncomeDto>().ReverseMap();
-            CreateMap<User, UserDto>().ReverseMap();
-            
-        
-        }
-    }
-}
-*/
 using AutoMapper;
 using DebtManagement.Web.DTOs;
 using DebtManagement.Web.Entities;
--- DebtManagement/DebtManagement.Web/Repositories/IClientRepository.cs
+++ DebtManagement/DebtManagement.Web/Repositories/IClientRepository.cs
@@ -1,19 +1,3 @@
-/*using DebtManagement.Web.Entities;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace DebtManagement.Web.Repositories
-{
-    public interface IClientRepository
-    {
-        Task<IEnumerable<User>> GetAllClientsAsync();
-        Task<User> GetClientByIdAsync(string clientId);
-        Task AddClientAsync(User client);
-        Task UpdateClientAsync(User client);
-        Task DeleteClientAsync(string clientId);
-    }
-}
-*/
 using DebtManagement.Web.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
