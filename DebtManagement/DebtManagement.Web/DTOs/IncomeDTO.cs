bf4543367eedaac9adc82c90dadc00845e0be4e5
bkaand <kaandurm@gmail.com>
2024-08-13 12:59:43 +0300

    income+payment implemented

--- DebtManagement/DebtManagement.Web/DTOs/IncomeDTO.cs
+++ DebtManagement/DebtManagement.Web/DTOs/IncomeDTO.cs
@@ -1,4 +1,4 @@
-namespace DebtManagement.Web.DTOs
+/*namespace DebtManagement.Web.DTOs
 {
     public class IncomeDto
     {
@@ -7,5 +7,17 @@ namespace DebtManagement.Web.DTOs
         public string AdditionalIncomeSources { get; set; }
         public DateTime RecordedDate { get; set; }
         public int ClientId { get; set; }
+    }
+}
+*/
+namespace DebtManagement.Web.DTOs
+{
+    public class IncomeDto
+    {
+        public int Id { get; set; }
+        public decimal MonthlyIncome { get; set; }
+        public string AdditionalIncomeSources { get; set; }
+        public DateTime RecordedDate { get; set; }
+        public string ClientId { get; set; }
     }
 }
--- DebtManagement/DebtManagement.Web/Mappings/MappingProfile.cs
+++ DebtManagement/DebtManagement.Web/Mappings/MappingProfile.cs
@@ -63,7 +63,7 @@ namespace DebtManagement.Web.Mapping
 }
 */
 
-using AutoMapper;
+/*using AutoMapper;
 using DebtManagement.Web.DTOs;
 using DebtManagement.Web.Entities;
 
@@ -94,3 +94,44 @@ namespace DebtManagement.Web.Mapping
         }
     }
 }
+*/
+using AutoMapper;
+using DebtManagement.Web.DTOs;
+using DebtManagement.Web.Entities;
+
+namespace DebtManagement.Web.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+
+            CreateMap<Client, ClientDTO>();
+            CreateMap<ClientDTO, Client>();
+
+            CreateMap<Debt, DebtDTO>()
+                .ForMember(dest => dest.ClientId, opt => opt.MapFrom(src => src.ClientId))
+                .ForMember(dest => dest.DebtType, opt => opt.MapFrom(src => src.DebtType));  
+
+            CreateMap<DebtDTO, Debt>()
+                .ForMember(dest => dest.ClientId, opt => opt.MapFrom(src => src.ClientId))
+                .ForMember(dest => dest.DebtType, opt => opt.MapFrom(src => src.DebtType));  
+
+            //CreateMap<Income, IncomeDto>();
+            //CreateMap<IncomeDto, Income>();
+            CreateMap<Income, IncomeDto>()
+                .ForMember(dest => dest.ClientId, opt => opt.MapFrom(src => src.ClientId));
+            CreateMap<IncomeDto, Income>()
+                .ForMember(dest => dest.ClientId, opt => opt.MapFrom(src => src.ClientId));
+
+            CreateMap<Payment, PaymentDTO>()
+                .ForMember(dest => dest.DebtAmount, opt => opt.MapFrom(src => src.Debt.DebtAmount.ToString()));
+
+            CreateMap<PaymentDTO, Payment>();
+        }
+    }
+}
+
+
