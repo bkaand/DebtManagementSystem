cbc7ce57b1e528a27da62414208a8aedb09ab7c7
bkaand <kaandurm@gmail.com>
2024-08-14 15:57:05 +0300

    calculator partially implemented

--- DebtManagement/DebtManagement.Web/Controllers/CalculatorController.cs
+++ DebtManagement/DebtManagement.Web/Controllers/CalculatorController.cs
@@ -30,6 +30,7 @@ namespace DebtManagement.Web.Controllers
 }
 */
 
+/*
 using AutoMapper;
 using DebtManagement.Web.DTOs;
 using DebtManagement.Web.Services;
@@ -73,6 +74,60 @@ namespace DebtManagement.Web.Controllers
             viewModel.TotalIncome = viewModel.Incomes.Sum(i => i.MonthlyIncome);
             viewModel.TotalPayments = viewModel.Payments.Sum(p => p.AmountPaid);
             viewModel.NetBalance = viewModel.TotalIncome - viewModel.TotalDebt;
+
+            // Populate Chart.js data
+            viewModel.IncomeLabels = viewModel.Incomes.Select(i => i.Source).ToList();
+            viewModel.IncomeValues = viewModel.Incomes.Select(i => i.MonthlyIncome).ToList();
+
+            viewModel.DebtLabels = viewModel.Debts.Select(d => d.DebtType.ToString()).ToList();
+            viewModel.DebtValues = viewModel.Debts.Select(d => d.DebtAmount).ToList();
+
+            viewModel.PaymentLabels = viewModel.Payments.Select(p => p.PaymentDate.ToString("MM/yyyy")).ToList();
+            viewModel.PaymentValues = viewModel.Payments.Select(p => p.AmountPaid).ToList();
+
+            return View(viewModel);
+        }
+    }
+}
+*/
+
+using AutoMapper;
+using DebtManagement.Web.DTOs;
+using DebtManagement.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtManagement.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly IDebtService _debtService;
+        private readonly IIncomeService _incomeService;
+        private readonly IPaymentService _paymentService;
+        private readonly IMapper _mapper;
+
+        public CalculatorController(IDebtService debtService, IIncomeService incomeService, IPaymentService paymentService, IMapper mapper)
+        {
+            _debtService = debtService;
+            _incomeService = incomeService;
+            _paymentService = paymentService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new CalculatorViewModel();
+
+            // Get data from services
+            var debts = await _debtService.GetAllDebtsAsync();
+            var incomes = await _incomeService.GetAllIncomesAsync();
+            var payments = await _paymentService.GetAllPaymentsAsync();
+
+            // Map data to DTOs
+            viewModel.Debts = _mapper.Map<List<DebtDTO>>(debts);
+            viewModel.Incomes = _mapper.Map<List<IncomeDto>>(incomes);
+            viewModel.Payments = _mapper.Map<List<PaymentDTO>>(payments);
 
             // Populate Chart.js data
             viewModel.IncomeLabels = viewModel.Incomes.Select(i => i.Source).ToList();
--- DebtManagement/DebtManagement.Web/DTOs/CalculatorViewModel.cs
+++ DebtManagement/DebtManagement.Web/DTOs/CalculatorViewModel.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 
 namespace DebtManagement.Web.DTOs
 {
@@ -16,6 +16,40 @@ namespace DebtManagement.Web.DTOs
         public List<PaymentDTO> Payments { get; set; }
 
         // Properties for Chart.js Data
+        public List<string> IncomeLabels { get; set; }
+        public List<decimal> IncomeValues { get; set; }
+        public List<string> DebtLabels { get; set; }
+        public List<decimal> DebtValues { get; set; }
+        public List<string> PaymentLabels { get; set; }
+        public List<decimal> PaymentValues { get; set; }
+
+        public CalculatorViewModel()
+        {
+            Debts = new List<DebtDTO>();
+            Incomes = new List<IncomeDto>();
+            Payments = new List<PaymentDTO>();
+
+            IncomeLabels = new List<string>();
+            IncomeValues = new List<decimal>();
+            DebtLabels = new List<string>();
+            DebtValues = new List<decimal>();
+            PaymentLabels = new List<string>();
+            PaymentValues = new List<decimal>();
+        }
+    }
+}
+*/
+
+using System.Collections.Generic;
+
+namespace DebtManagement.Web.DTOs
+{
+    public class CalculatorViewModel
+    {
+        public List<DebtDTO> Debts { get; set; }
+        public List<IncomeDto> Incomes { get; set; }
+        public List<PaymentDTO> Payments { get; set; }
+
         public List<string> IncomeLabels { get; set; }
         public List<decimal> IncomeValues { get; set; }
         public List<string> DebtLabels { get; set; }
