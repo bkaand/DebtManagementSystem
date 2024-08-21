4fba86c7a5d85c8b0f5a0c3e4af22e323640bd36
bkaand <kaandurm@gmail.com>
2024-08-21 12:55:09 +0300

    clientid removed from debts

--- DebtManagement/DebtManagement.Web/Controllers/DebtsController.cs
+++ DebtManagement/DebtManagement.Web/Controllers/DebtsController.cs
@@ -1,5 +1,5 @@
 ﻿/* This one works, also dont forget to remove ClientId from add and edit?. Auto fill it with the current ClientId. Same applies with payment and income and graph. Also installments are wrong.*/
-using AutoMapper; 
+/*using AutoMapper; 
 using DebtManagement.Web.DTOs;
 using DebtManagement.Web.Services;
 using Microsoft.AspNetCore.Identity;
@@ -109,6 +109,167 @@ namespace DebtManagement.Web.Controllers
             {
                 var debt = _mapper.Map<Debt>(debtDto);
                 debt.ClientId = _userManager.GetUserId(User); 
+                await _debtService.UpdateDebtAsync(debt);
+                return RedirectToAction(nameof(Index));
+            }
+
+            var clients = await _clientService.GetAllClientsAsync();
+            ViewBag.Clients = new SelectList(clients, "Id", "Name");
+
+            return View(debtDto);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var debt = await _debtService.GetDebtByIdAsync(id);
+            if (debt == null)
+            {
+                return NotFound();
+            }
+            var debtDto = _mapper.Map<DebtDTO>(debt);
+            return View(debtDto);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var debt = await _debtService.GetDebtByIdAsync(id);
+            if (debt == null)
+            {
+                return NotFound();
+            }
+            var debtDto = _mapper.Map<DebtDTO>(debt);
+            return View(debtDto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _debtService.DeleteDebtAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+*/
+using AutoMapper;
+using DebtManagement.Web.DTOs;
+using DebtManagement.Web.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DebtManagement.Web.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DebtManagement.Web.Entities.Enums;
+
+namespace DebtManagement.Web.Controllers
+{
+    public class DebtsController : Controller
+    {
+        private readonly IDebtService _debtService;
+        private readonly IClientService _clientService;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public DebtsController(
+            IDebtService debtService,
+            IClientService clientService,
+            IMapper mapper,
+            UserManager<User> userManager)
+        {
+            _debtService = debtService;
+            _clientService = clientService;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = user?.Id;
+
+            DebtsViewModel viewModel = new DebtsViewModel
+            {
+                CreditCardDebts = _mapper.Map<List<DebtDTO>>(await _debtService.GetDebtsByClientIdAndTypeAsync(userId, DebtType.CreditCard)),
+                LoanDebts = _mapper.Map<List<DebtDTO>>(await _debtService.GetDebtsByClientIdAndTypeAsync(userId, DebtType.Loan)),
+                AvansDebts = _mapper.Map<List<DebtDTO>>(await _debtService.GetDebtsByClientIdAndTypeAsync(userId, DebtType.Avans)),
+                MonthlyRentsDebts = _mapper.Map<List<DebtDTO>>(await _debtService.GetDebtsByClientIdAndTypeAsync(userId, DebtType.MonthlyRents)),
+                OtherDebts = _mapper.Map<List<DebtDTO>>(await _debtService.GetDebtsByClientIdAndTypeAsync(userId, DebtType.Other))
+            };
+
+            viewModel.IsEmpty = !viewModel.CreditCardDebts.Any() &&
+                                !viewModel.LoanDebts.Any() &&
+                                !viewModel.AvansDebts.Any() &&
+                                !viewModel.MonthlyRentsDebts.Any() &&
+                                !viewModel.OtherDebts.Any();
+
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var clientId = user?.Id;
+            ViewBag.ClientId = clientId;
+
+            var clients = await _clientService.GetAllClientsAsync();
+            ViewBag.Clients = new SelectList(clients, "Id", "Name", clientId); // Automatically select the current user
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DebtDTO debtDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var clientId = user?.Id;
+
+                var debt = _mapper.Map<Debt>(debtDto);
+                debt.ClientId = clientId; // Assign the current user's client ID
+                await _debtService.AddDebtAsync(debt);
+                return RedirectToAction(nameof(Index));
+            }
+
+            var clients = await _clientService.GetAllClientsAsync();
+            ViewBag.Clients = new SelectList(clients, "Id", "Name");
+
+            return View(debtDto);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var debt = await _debtService.GetDebtByIdAsync(id);
+            if (debt == null)
+            {
+                return NotFound();
+            }
+
+            var clients = await _clientService.GetAllClientsAsync();
+            ViewBag.Clients = new SelectList(clients, "Id", "Name");
+
+            var debtDto = _mapper.Map<DebtDTO>(debt);
+            return View(debtDto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, DebtDTO debtDto)
+        {
+            if (id != debtDto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var clientId = user?.Id;
+
+                var debt = _mapper.Map<Debt>(debtDto);
+                debt.ClientId = clientId; // Ensure the ClientId is the current user's ID
                 await _debtService.UpdateDebtAsync(debt);
                 return RedirectToAction(nameof(Index));
             }
